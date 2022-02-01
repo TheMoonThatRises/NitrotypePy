8d368e9c1ba61fa8fd087f952be135956d5b1366
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2022-01-31 21:08:16 -0700

    test

--- NitrotypePy/__init__.py
+++ NitrotypePy/__init__.py
@@ -1,3 +1,3 @@
-from api.info.bootstrap.items import car, loot
-from api.info.bootstrap import bootstrap
-from api.info import user
+from .api.info.bootstrap.items import car, loot
+from .api.info.bootstrap import bootstrap
+from .api.info import user
