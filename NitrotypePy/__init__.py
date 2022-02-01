fa686c91a9c085460e7ae79040ec99b06a5c38cb
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2022-02-01 14:25:27 -0700

    Fixed imports

--- NitrotypePy/__init__.py
+++ NitrotypePy/__init__.py
@@ -1,3 +1,18 @@
-from .api.info.bootstrap.items import car, loot
-from .api.info.bootstrap import bootstrap
-from .api.info import user
+# Include apiv2 endpoints
+from .api.apiv2.endpoints.leaderboard import leaderboard
+from .api.apiv2.endpoints.teams import teams
+from .api.apiv2.endpoints.news import news
+
+# Include bootstrap items
+from .api.info.bootstrap.items.car import car
+from .api.info.bootstrap.items.loot import loot
+
+# Include bootstrap misc items
+from .api.info.bootstrap.misc.challenges import challenges
+from .api.info.bootstrap.misc.top import top
+
+# Include entire bootstrap
+from .api.info.bootstrap.bootstrap import bootstrap
+
+# Include user information
+from .api.info.user import user
