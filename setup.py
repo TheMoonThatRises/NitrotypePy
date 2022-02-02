b1090b2b644012e0a6c80a1c14181050d7925233
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2022-02-01 18:19:28 -0700

    Fix namespace package import issue

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(name='NitrotypePy',
     version='0.0.2',
     description='A way to access nitrotype\'s unoffical api',
     author='The Moon That Rises',
     url='https://www.github.com/RangerEmerald/NitrotypePy',
-    packages = find_packages(),
+    packages = find_namespace_packages(include = 'NitrotypePy.*'),
     install_requires = [
         'cloudscraper >= 1.2.58',
     ],
