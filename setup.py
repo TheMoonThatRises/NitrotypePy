203bb4646ed6ce0760ed308680ed4e5eaf2bbd1e
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2022-02-01 17:54:57 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='NitrotypePy',
-    version='0.0.1',
+    version='0.0.2',
     description='A way to access nitrotype\'s unoffical api',
     author='The Moon That Rises',
     url='https://www.github.com/RangerEmerald/NitrotypePy',
