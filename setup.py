4e9b190f34c96857051919a8e346ef0e3f75aaa1
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2022-02-03 20:42:20 -0700

    Bump package version

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='NitrotypePy',
-      version='0.0.2',
+      version='0.0.3',
       description='A way to access nitrotype\'s unoffical api',
       author='The Moon That Rises',
       url='https://www.github.com/RangerEmerald/NitrotypePy',
       packages=find_namespace_packages(include='NitrotypePy.*'),
       install_requires=[
           'cloudscraper >= 1.2.58',
-      ],
-      )
+      ])
